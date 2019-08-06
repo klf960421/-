e16f07c2a2fb022d6444d93ef1a181e5a96f310a
Klf <klf960421@163.com>
2019-08-06 17:24:38 +0800

    搭建目录结构

---
+++ src/pages/home/home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        home
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
